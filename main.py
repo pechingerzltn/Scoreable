b0e23cb30282d873e3cf581d6cdcd9337fa22537
pechingerzltn <pechingerzltn@gmail.com>
2022-09-24 22:28:10 +0200

    creating index.html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 
 app = Flask(__name__)
@@ -7,12 +7,35 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/login/")
+@app.route("/login/", methods=["GET", "POST"])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
-@app.route("/register/")
+@app.route("/register/", methods=["GET", "POST"])
 def register():
+
+    """
+    if request.method == "POST":
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+    
+    if "@" not in email:
+        flash("Email is invalid.", category="error")
+    elif len(username) < 4:
+        flash("Username must be longer than 3 characters", category="error")
+    elif len(password) < 8:
+        flash("Password must be at least 8 characters long", category="error")
+    elif password != confirm_password:
+        flash("Passwords do not match", category="error")
+    else:
+        flash("Successful.", category="success")
+        pass
+    """
+
     return render_template("register.html")
 
 if __name__ == '__main__':
